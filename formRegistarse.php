fd47d45ad5c4b8a8ec67b0b5b0dd771da9f7eaec
Alejandro Manzo <nanommfp@gmail.com>
2022-11-26 18:17:35 +0100

    funcion para que las contraseñas sean iguales

--- formRegistarse.php
+++ formRegistarse.php
@@ -2,18 +2,22 @@
     require "./conexion.php";
     require "./includes/header.php";
     require "./registrarUsuario.php";
-    if (!empty($message)):?>
-        <p><?= $message?></p>
-<?php endif; ?>
+?>
 
-<h1>Registarte</h1>
+<div class="contenedorPrincipal-formRegistrarse">
+    <?php 
+        if (!empty($message)):?>
+            <p class="alert alert-danger mensaje"><?= $message?></p>
+    <?php endif; ?>
+    <h1>Registarte</h1>
     <span>O <a href="./index.php">inicia sesión</a></span>
     <form action="./formRegistarse.php" method="post">
         <input type="email" name="email" placeholder="Ingrese su email">
-        <input type="password" name="password" placeholder="Ingrese su contraseña">
-        <input type="password" name="confirm_password" placeholder="Confirme su contraseña">
-        <input type="submit" value="Enviar">
+        <input type="password" id="password" name="password" placeholder="Ingrese su contraseña">
+        <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirme su contraseña">
+        <input type="submit" id="enviar-formRegistrarse" value="Enviar">
     </form>
+</div>
 
 <?php
     require "./includes/foother.php"
--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 let menuHamburguesa = document.querySelector(".menu");
 let mobileMenu = document.querySelector(".mobile-menu")
+let password = document.querySelector("#password")
+let confirmPassword = document.querySelector("#confirm_password")
+let enviarformRegistrarse = document.querySelector("#enviar-formRegistrarse")
+
+
 
 menuHamburguesa.addEventListener("click", toggleBurguerMenu);
+enviarformRegistrarse.addEventListener("click", validarPassword);
 
 function toggleBurguerMenu() {
     mobileMenu.classList.toggle("inactivo");
     console.log("hola")
 }
+
+function validarPassword(event) {
+    if (confirmPassword.value != password.value) {
+        event.preventDefault();
+        alert("las contraseñas deben ser iguales");
+      }
+
+} 
+
