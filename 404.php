89ce96ec4a3342a20e01a281755f1fda6f5688e9
Alejandro Manzo <nanommfp@gmail.com>
2022-12-03 00:06:10 +0100

    pagina 404

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php
+    require "includes/header.php";
+?>
+
+<div class="contenedorPrincipal-404">
+        <div class="contenedorImagen-404">
+            <img src="img/triangulo-advertencia.png" alt="">
+        </div>
+        <div>
+            <p>No hemos encontrado la pagina que buscas</p>
+            <p>Puedes volver a la <a href="./index.php">Pagina Principal</a></p>
+        </div>
+    </div>
+
+<?php
+    require "includes/foother.php"
+?>
