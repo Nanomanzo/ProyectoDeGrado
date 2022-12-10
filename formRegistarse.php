708f1edc796e52a60817339a6c41ae6b77848e93
Alejandro Manzo <nanommfp@gmail.com>
2022-12-10 17:54:03 +0100

    estilos fomularios

--- formRegistarse.php
+++ formRegistarse.php
@@ -11,7 +11,10 @@
     <?php endif; ?>
     <h1>Registarte</h1>
     <span>O <a href="./index.php">inicia sesión</a></span>
-    <form action="./formRegistarse.php" method="post">
+    <form class="formRegistrarse" action="./formRegistarse.php" method="post">
+        <input type="text" name="nombre" placeholder="Ingrese su nombre">
+        <input type="text" name="apellido" placeholder="Ingrese su apellido">
+        <input type="text" name="nick_name" placeholder="Ingrese su nickname">
         <input type="email" name="email" placeholder="Ingrese su email">
         <input type="password" id="password" name="password" placeholder="Ingrese su contraseña">
         <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirme su contraseña">
